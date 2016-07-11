43eee6c55badbd8e9578b33c9736fae5a97e27ba
berk ozdilek <biozdilek@gmail.com>
2016-07-11 15:15:51 +0300

    version updated

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ def readme():
 
 
 setup(name='ivencloud',
-      version='0.1.4',
+      version='0.1.5',
       url='https://github.com/BerkOzdilek/PythonSDK',
       author='Berk Ozdilek',
       author_email='biozdilek@gmail.com',
